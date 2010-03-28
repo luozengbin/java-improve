a6749f1429f4baa4e872fd7fc7bf03c3f3840517
jalen.cn <jalen.cn@5adb9879-7f9d-1759-7037-7e960cc2ad8d>
2010-03-28 13:57:52 +0000

    Share project "javaenhance" into "https://java-improve.googlecode.com/svn"

---
+++ javaenhance/src/skillup/java/basic/TestEnumAsSingleton.java
@@ -0,0 +1,88 @@
+package skillup.java.basic;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class TestEnumAsSingleton {
+	
+	@Test
+	public void testSingleton() throws Exception {
+		
+		System.out.println(アキラ.妻.getValue());
+		
+		System.out.println(アキラ.妻.getValue());
+		
+		assertEquals(アキラ.妻, アキラ.妻);
+		
+	}
+	
+	
+	//インスタンス１つしかありえないので
+	public enum アキラ{
+		
+		妻;
+		
+		アキラ(){
+			_妻 = new TestEnumAsSingleton().new Person(20, "米蘭", Sex.女性);
+		}
+		
+		private Person _妻;
+		
+		public Person getValue(){
+			return _妻;
+		}
+	}
+	
+	public enum Sex{
+		男性,
+		女性,
+		ニューハフ;
+	}
+	
+	public class Person{
+		
+		
+		private int age;
+		
+		private String name;
+		
+		private Sex sex;
+		
+		public Person() {
+			super();
+		}
+
+		public Person(int age, String name, Sex sex) {
+			super();
+			this.age = age;
+			this.name = name;
+			this.sex = sex;
+		}
+
+		public int getAge() {
+			return age;
+		}
+
+		public void setAge(int age) {
+			this.age = age;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public Sex getSex() {
+			return sex;
+		}
+
+		public void setSex(Sex sex) {
+			this.sex = sex;
+		}
+	}
+}
