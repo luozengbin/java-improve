5db3e18cea1d8cfd9dcbc592e1321cde077aea9d
jalen.cn <jalen.cn@5adb9879-7f9d-1759-7037-7e960cc2ad8d>
2010-03-30 05:43:25 +0000

    commit change at oly

--- rmi-consumer/src/rmi/RmiSampleClient.java
+++ rmi-consumer/src/rmi/RmiSampleClient.java
@@ -9,7 +9,15 @@ public class RmiSampleClient {
 
 	public static void main(String[] args) throws MalformedURLException, RemoteException, NotBoundException {
 		
+		//java.rmi.Naming#lookup()で、RMIレジストリー（デフォルトでポート1099）からリモートオブジェクトのインターフェースを取得する。
 		RmiSample rmiSample = (RmiSample)Naming.lookup(RmiSample.SERVICE_NAME);
+		
+		//もしくはRegistryを使って取得する。（というか、Namingの内部はRegistryを使っている模様）
+		/*
+		Registry registry = LocateRegistry.getRegistry(Registry.REGISTRY_PORT);
+		Remote r = registry.lookup("RmiSampleName");
+		*/
+		
 		System.out.println(rmiSample.getMessage());
 		
 	}
