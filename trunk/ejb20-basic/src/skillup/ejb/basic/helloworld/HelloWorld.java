e33a7cb55c6469911dc5b331fc70ad0239d8e8e4
jalen.cn <jalen.cn@5adb9879-7f9d-1759-7037-7e960cc2ad8d>
2010-04-05 09:42:46 +0000

    fix build script and Refactoring foldar struct

--- trunk/ejb20-basic/src/skillup/ejb/basic/helloworld/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package skillup.ejb.basic.helloworld;
-
-import java.rmi.RemoteException;
-
-import javax.ejb.EJBObject;
-
-public interface HelloWorld extends EJBObject {
-	public String sayHelloWorld() throws RemoteException;
-}
