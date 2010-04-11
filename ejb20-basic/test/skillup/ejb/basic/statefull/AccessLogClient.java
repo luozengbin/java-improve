3c6f67b959bac7edaebc97a7a355969422891f10
jalen.cn <jalen.cn@5adb9879-7f9d-1759-7037-7e960cc2ad8d>
2010-04-11 15:00:27 +0000

    at home

--- ejb20-basic/test/skillup/ejb/basic/statefull/AccessLogClient.java
+++ ejb20-basic/test/skillup/ejb/basic/statefull/AccessLogClient.java
@@ -1,6 +1,8 @@
 package skillup.ejb.basic.statefull;
 
 import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.ejb.CreateException;
 import javax.ejb.RemoveException;
@@ -21,10 +23,6 @@ public class AccessLogClient {
 		AccessLogHome home = (AccessLogHome) PortableRemoteObject.narrow(ctx.lookup("AccessLog"), AccessLogHome.class);
 		ctx.close();
 		
-		log.debug("getEJBMetaData:" + home.getEJBMetaData());
-		
-		
-		
 		AccessLog accessLog_1 = home.createWithURL("http://www.google.com");
 		AccessLog accessLog_2 = home.createWithURL("http://www.google.com");
 		
@@ -40,5 +38,16 @@ public class AccessLogClient {
 		//beanのresource管理
 		
 		//bean-cache設定の効果
+		
+		List<AccessLog> accessLogList = new ArrayList<AccessLog>(); 
+		
+		for (int i = 0; i < 10; i++) {
+			accessLogList.add(home.createWithURL("http://www.google.com"));
+			
+		}
+		
+		for (AccessLog accessLog : accessLogList) {
+			log.debug(accessLog.getAccessHistory());
+		}
 	}
 }
