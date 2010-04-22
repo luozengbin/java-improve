430029999de73337d8466c10c8c207ee4ccec7b4
jalen.cn <jalen.cn@5adb9879-7f9d-1759-7037-7e960cc2ad8d>
2010-04-22 04:15:41 +0000

    delete unusefull file

--- ejb30-basic/src/skillup/ejb30/basic/helloworld/HelloWorldLocal.java
+++
@@ -1,4 +0,0 @@
-package skillup.ejb30.basic.helloworld;
-
-public interface HelloWorldLocal extends HelloWorld {
-}
--- ejb30-basic/src/skillup/ejb30/basic/other/Other.java
+++
@@ -1,5 +0,0 @@
-package skillup.ejb30.basic.other;
-
-public interface Other {
-	public String sayMe();
-}
